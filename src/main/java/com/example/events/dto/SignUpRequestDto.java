6d18fcc48f72814c8a96f1ffda771533b05017b9
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-26 22:05:00 +0300

    implement Rate limit

--- src/main/java/com/example/events/dto/SignUpRequestDto.java
+++ src/main/java/com/example/events/dto/SignUpRequestDto.java
@@ -1,5 +1,8 @@
 package com.example.events.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Data
@@ -8,9 +11,20 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class SignUpRequestDto {
+
+    @NotNull(message = "Name should not be null")
+    @NotBlank(message = "Name should not be null")
     private String firstName;
+    @NotNull(message = "Name should not be null")
+    @NotBlank(message = "Name should not be null")
     private String lastName;
+    @NotBlank(message = "Name should not be null")
+    @Email(message = "Email should be valid")
     private String email;
+    @NotNull(message = "Password should not be blank")
     private String password;
+    @NotBlank(message = "Name should not be null")
+    @NotNull(message = "Include phone Number")
+    private String phone;
 
 }
