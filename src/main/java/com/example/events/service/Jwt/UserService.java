0042f15b5dd472456a3c9cabf85318380b3fbe10
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-26 19:07:23 +0300

    event starter project,register,event,user functions

---
+++ src/main/java/com/example/events/service/Jwt/UserService.java
@@ -0,0 +1,8 @@
+package com.example.events.service.Jwt;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService {
+    UserDetailsService userDetailsService();
+
+}
