67a1294e500860b5ccf6a4745fb566efd057f3a7
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-26 22:04:56 +0300

    implement Rate limit

--- src/main/java/com/example/events/dto/JWTAuthenticationResponse.java
+++ src/main/java/com/example/events/dto/JWTAuthenticationResponse.java
@@ -1,8 +1,12 @@
 package com.example.events.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class JWTAuthenticationResponse {
     private String token;
     private String refreshToken;
