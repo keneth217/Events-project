eaac66ba9067e963b5cf460bb35929f16f0d8e03
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-26 19:07:26 +0300

    event starter project,register,event,user functions

---
+++ src/main/java/com/example/events/service/Jwt/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.events.service.Jwt;
+
+
+
+
+import com.example.events.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements  UserService{
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return userRepository.findByEmail(username)
+                        .orElseThrow(()-> new UsernameNotFoundException("User Not found"));
+            }
+        };
+    }
+}
