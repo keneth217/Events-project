5d7ae6cc034c5f75a48532b10c2410d1c60306cf
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-26 22:04:41 +0300

    implement Rate limit

--- src/main/java/com/example/events/controller/AuthenticationController.java
+++ src/main/java/com/example/events/controller/AuthenticationController.java
@@ -1,9 +1,6 @@
 package com.example.events.controller;
 
-
-
-
-import com.example.events.configs.JwtAuthenticationFilter;
+import com.example.events.configs.RateLimitingService;
 import com.example.events.constants.AuthConstants;
 import com.example.events.dto.AuthenticationRequestDto;
 import com.example.events.dto.JWTAuthenticationResponse;
@@ -11,42 +8,44 @@ import com.example.events.dto.ResponseDto;
 import com.example.events.dto.SignUpRequestDto;
 import com.example.events.service.Auth.AuthService;
 import com.example.events.service.Jwt.UserService;
-import com.example.events.user.User;
-import com.example.events.user.UserRepository;
+
 import com.example.events.util.JwtUtils;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
-
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.*;
-
-
 
 @RestController
 @RequestMapping("api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
+
     private final AuthService authService;
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final AuthenticationManager authenticationManager;
     private final UserService userService;
     private final JwtUtils jwtUtil;
-    private final UserRepository userRepository;
+    private final RateLimitingService rateLimitingService;
 
     @PostMapping("/sign")
-    public ResponseEntity<ResponseDto> createUser(@Valid @RequestBody SignUpRequestDto signUpRequest){
+    public ResponseEntity<ResponseDto> createUser(@Valid @RequestBody SignUpRequestDto signUpRequest, HttpServletRequest request) {
+        // Check if the request is within the rate limit
+        rateLimitingService.checkRateLimit(request);
+
         authService.createUser(signUpRequest);
-        String userName= signUpRequest.getFirstName()+" "+signUpRequest.getLastName();
+        String userName = signUpRequest.getFirstName() + " " + signUpRequest.getLastName();
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(new ResponseDto(AuthConstants.ACCOUNT_CREATION_CODE,userName+" "+ AuthConstants.ACCOUNT_CREATION));
-//        return  new ResponseEntity<>("HELLO"+" "+userName+" "+"ACCOUNT CREATION SUCCESS,WELCOME TO OUR E-COMMERCE SERVICE",HttpStatus.CREATED);
+                .body(new ResponseDto(AuthConstants.ACCOUNT_CREATION_CODE, userName + " " + AuthConstants.ACCOUNT_CREATION));
     }
+
     @PostMapping("/login")
-    public ResponseEntity<JWTAuthenticationResponse> login(@RequestBody AuthenticationRequestDto authenticationRequest){
+    public ResponseEntity<JWTAuthenticationResponse> login(@RequestBody AuthenticationRequestDto authenticationRequest, HttpServletRequest request) {
+        // Check if the request is within the rate limit
+        rateLimitingService.checkRateLimit(request);
+
         return ResponseEntity.ok(authService.createAuthToken(authenticationRequest));
     }
 }
