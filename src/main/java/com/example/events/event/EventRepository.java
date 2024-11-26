3afb0b7e686d78b29fd43f229fe14cc9c8a475b2
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-26 19:07:44 +0300

    event starter project,register,event,user functions

---
+++ src/main/java/com/example/events/event/EventRepository.java
@@ -0,0 +1,8 @@
+package com.example.events.event;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface EventRepository extends JpaRepository<MyEvent, UUID> {
+}
