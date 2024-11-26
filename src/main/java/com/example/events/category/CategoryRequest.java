42d2058ccf18f7317b41cbed788170cf3cbe2caf
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-26 19:06:41 +0300

    event starter project,register,event,user functions

---
+++ src/main/java/com/example/events/category/CategoryRequest.java
@@ -0,0 +1,18 @@
+package com.example.events.category;
+
+
+import lombok.Builder;
+import lombok.Data;
+import java.util.UUID;
+@Data
+@Builder
+public class CategoryRequest {
+
+
+    private UUID id;
+    private String categoryName;
+    private String categoryDescription;
+
+
+
+}
