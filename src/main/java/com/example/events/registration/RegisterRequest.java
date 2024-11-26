57050c3fa8eccf819374150ad10f58ca05e66efb
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-26 19:07:53 +0300

    event starter project,register,event,user functions

---
+++ src/main/java/com/example/events/registration/RegisterRequest.java
@@ -0,0 +1,20 @@
+package com.example.events.registration;
+
+import com.example.events.event.MyEvent;
+import com.example.events.user.User;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.UUID;
+
+@Data
+@Builder
+public class RegisterRequest {
+    private LocalDate regDate;
+    private LocalTime regTime;
+    private UUID userId;
+    private UUID eventId;
+}
