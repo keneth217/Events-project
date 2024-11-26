d3e90069fc83a17454c85fab216e4b27dd7b108c
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-26 19:08:27 +0300

    event starter project,register,event,user functions

---
+++ src/main/java/com/example/events/user/User.java
@@ -0,0 +1,81 @@
+package com.example.events.user;
+
+
+
+import com.example.events.enums.Role;
+import com.example.events.registration.EventRegistration;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @OneToMany(mappedBy = "user")
+    private List<EventRegistration> registrations;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return phone;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Repository
+    public static interface UserRepository extends JpaRepository<User,Long> {
+        Optional<User> findByEmail(String email);
+        User findByRole(Role role);
+
+    //    User findFirstByEmail(String email);
+    }
+}
+
+
+
+
