e17b8fdc51ab2a23302c08dabff6aef8922e30e0
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-27 21:44:03 +0300

    fix register for event,send email,add authentications

--- src/main/java/com/example/events/category/CategoryController.java
+++ src/main/java/com/example/events/category/CategoryController.java
@@ -1,11 +1,15 @@
 package com.example.events.category;
 
+import com.example.events.configs.RateLimitingService;
 import com.example.events.event.EventRequest;
 import com.example.events.event.EventResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,6 +18,7 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class CategoryController {
     private final CategoryService categoryService;
+    private final RateLimitingService rateLimitingService;
 
     @PostMapping
     public ResponseEntity<CategoryResponse> createCategory(@RequestBody CategoryRequest categoryRequest){
@@ -38,17 +43,38 @@ public class CategoryController {
         }
         return ResponseEntity.ok(category);
     }
-    @DeleteMapping("/{categoryId}")
+    @DeleteMapping("/delete/{categoryId}")
     public ResponseEntity<String> deleteCategory(@PathVariable UUID categoryId) {
         categoryService.deleteCategory(categoryId);
 
         return  ResponseEntity.ok("Event Deleted successfully" );
     }
 
-    @PutMapping("/{eventId}")
+    @PutMapping("/update/{categoryId}")
     public ResponseEntity<CategoryResponse> updateEvent(@PathVariable UUID categoryId, @RequestBody CategoryRequest  categoryRequest){
         return ResponseEntity.ok(categoryService.updateCategory(categoryId,categoryRequest));
     }
+
+    @GetMapping("/events/{categoryName}")
+    public ResponseEntity<List<CategoryEventResponse>> filterEventsByCategory(
+            @PathVariable String categoryName,
+            HttpServletRequest request
+    ) {
+        // Check if the request is within the rate limit
+        rateLimitingService.checkRateLimit(request);
+
+        // Fetch events within the date range
+        CategoryEventResponse allEvents = categoryService.filterEventByCategoryName(categoryName);
+        // If no events found, return 204 No Content
+        if (allEvents == null) {
+            return ResponseEntity.noContent().build();
+        }
+
+        // Return the list of events with 200 OK
+        return ResponseEntity.ok(Collections.singletonList(allEvents));
+    }
+
+
 
 
 }
