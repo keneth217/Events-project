eb46d475812a6585dcb62e552d7a9f6d355e7860
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-27 21:43:42 +0300

    fix register for event,send email,add authentications

---
+++ src/main/java/com/example/events/emails/EmailDetails.java
@@ -0,0 +1,20 @@
+package com.example.events.emails;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailDetails {
+    private String recipient;
+    private String messageBody;
+    private String subject;
+    private String attachment;
+    private List<String> customerEmails;
+}
