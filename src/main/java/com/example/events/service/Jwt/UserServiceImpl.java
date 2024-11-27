66fbf6d4b7ae0f98b79c1db74862a03c4b181139
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-27 21:44:52 +0300

    fix register for event,send email,add authentications

--- src/main/java/com/example/events/service/Jwt/UserServiceImpl.java
+++ src/main/java/com/example/events/service/Jwt/UserServiceImpl.java
@@ -19,9 +19,9 @@ public class UserServiceImpl implements  UserService{
     public UserDetailsService userDetailsService() {
         return new UserDetailsService() {
             @Override
-            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-                return userRepository.findByEmail(username)
-                        .orElseThrow(()-> new UsernameNotFoundException("User Not found"));
+            public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+                return userRepository.findByEmail(email)
+                        .orElseThrow(()-> new UsernameNotFoundException("User Not found with the given credentials "));
             }
         };
     }
