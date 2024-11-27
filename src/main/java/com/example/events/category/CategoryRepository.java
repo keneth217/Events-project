aa15c564a2aa11b3f9783b275197409ea317aa85
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-27 21:44:11 +0300

    fix register for event,send email,add authentications

--- src/main/java/com/example/events/category/CategoryRepository.java
+++ src/main/java/com/example/events/category/CategoryRepository.java
@@ -1,8 +1,14 @@
 package com.example.events.category;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CategoryRepository extends JpaRepository<EventCategory, UUID> {
+   Optional<EventCategory>  findByCategoryName(String categoryName);
+   @Query("SELECT c FROM EventCategory c WHERE LOWER(TRIM(c.categoryName)) = LOWER(TRIM(:categoryName))")
+   Optional<EventCategory> findByCategoryNameIgnoringCaseAndSpaces(@Param("categoryName") String categoryName);
 }
