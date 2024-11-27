f59e405028e0f3e85ff19f3843492e34aa6eb854
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-27 21:43:53 +0300

    fix register for event,send email,add authentications

--- src/main/java/com/example/events/registration/RegistrationRepository.java
+++ src/main/java/com/example/events/registration/RegistrationRepository.java
@@ -5,10 +5,17 @@ import com.example.events.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface RegistrationRepository extends JpaRepository<EventRegistration, UUID> {
     boolean existsByUserAndEvent(User user, MyEvent event);
 
     List<EventRegistration> findByEvent(MyEvent event);
+
+    int countByEvent(MyEvent event);
+
+    List<EventRegistration> findByUser(User user);
+
+    Optional<EventRegistration> findByUniqueCode(String qrCodeContent);
 }
