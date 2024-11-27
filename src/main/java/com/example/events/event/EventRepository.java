4edd994293c6b74f811d463786b2a60f5d4e483d
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-27 21:43:06 +0300

    fix register for event,send email,add authentications

--- src/main/java/com/example/events/event/EventRepository.java
+++ src/main/java/com/example/events/event/EventRepository.java
@@ -2,7 +2,13 @@ package com.example.events.event;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 public interface EventRepository extends JpaRepository<MyEvent, UUID> {
+
+    List<MyEvent> findByStartDateBetween(LocalDate startDate, LocalDate endDate);
+
+
 }
