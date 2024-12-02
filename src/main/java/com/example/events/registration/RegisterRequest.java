99703316b901c1c9160abc4e57823472f195b139
keneth/217 <kipyegonkeneth03@gmail.com>
2024-12-02 20:26:50 +0300

    add paytments,fix response,upload image

--- src/main/java/com/example/events/registration/RegisterRequest.java
+++ src/main/java/com/example/events/registration/RegisterRequest.java
@@ -1,6 +1,7 @@
 package com.example.events.registration;
 
 import com.example.events.event.MyEvent;
+import com.example.events.payments.Payment;
 import com.example.events.user.User;
 import jakarta.persistence.*;
 import lombok.Builder;
@@ -9,6 +10,7 @@ import lombok.Data;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -21,8 +23,12 @@ public class RegisterRequest {
     private boolean scanned;
     private String transactionId;
     private Long ticketQuantity;
-
+    private BigDecimal amount;
     private BigDecimal paidAmount;
     private BigDecimal eventCost;
     private BigDecimal remainingAmount;
+
+    // One Ticket can have many Payments
+    @OneToMany(mappedBy = "ticket", cascade = CascadeType.ALL)
+    private List<Payment> payments; // List of payments for this ticket
 }
