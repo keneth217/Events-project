cc46f5bea0444564ad8ed5bf19a1e4fa5e045190
keneth/217 <kipyegonkeneth03@gmail.com>
2024-12-02 20:26:27 +0300

    add paytments,fix response,upload image

---
+++ src/main/java/com/example/events/payments/PaymentRepository.java
@@ -0,0 +1,21 @@
+package com.example.events.payments;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+public interface PaymentRepository extends JpaRepository<Payment,Long> {
+    Payment findByReference(String reference);
+
+
+    @Query("SELECT t.status FROM Payment t WHERE t.reference = :reference")
+    String findStatusByReference(@Param("reference") String reference);
+
+    // Repository method for fetching successful payments for a given transaction ID
+    List<Payment> findByTicket_RegistrationIdAndStatus(UUID registrationId, String status);
+
+}
