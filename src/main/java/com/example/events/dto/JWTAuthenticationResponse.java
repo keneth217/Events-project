2baed714c3c3b7f54ba04203a102187839fa93e6
keneth/217 <kipyegonkeneth03@gmail.com>
2024-12-18 20:44:24 +0300

    fix login response

--- src/main/java/com/example/events/dto/JWTAuthenticationResponse.java
+++ src/main/java/com/example/events/dto/JWTAuthenticationResponse.java
@@ -1,15 +1,19 @@
 package com.example.events.dto;
 
+
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class JWTAuthenticationResponse {
     private String token;
     private String refreshToken;
+    private UserDetailDto user;
 
 
 }
