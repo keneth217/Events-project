71eeeaa2bfe30493c602ebe488c1d93053488bfc
keneth/217 <kipyegonkeneth03@gmail.com>
2024-12-19 22:36:46 +0300

    fix ticket response,refresh token

--- src/main/java/com/example/events/user/UserRepository.java
+++ src/main/java/com/example/events/user/UserRepository.java
@@ -2,6 +2,7 @@ package com.example.events.user;
 
 import com.example.events.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -9,4 +10,7 @@ import java.util.UUID;
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
     User findByRole(Role role);
+
+    @Query("SELECT u FROM User u JOIN FETCH u.registrations WHERE u.id = :userId")
+    Optional<User> findUserWithRegistrations(UUID userId);
 };
