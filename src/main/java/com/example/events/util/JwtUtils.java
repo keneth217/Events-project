c02433318bc0dbe7894ff2eca89ffca5fefe2cf1
keneth/217 <kipyegonkeneth03@gmail.com>
2024-12-19 22:36:55 +0300

    fix ticket response,refresh token

--- src/main/java/com/example/events/util/JwtUtils.java
+++ src/main/java/com/example/events/util/JwtUtils.java
@@ -17,50 +17,54 @@ import java.util.function.Function;
 @Component
 public class JwtUtils {
 
-    // Secret key for signing JWT tokens
     private static final String SECRET = "DGEF7R3R9404I34R8R23U8FWQLDNQWDWQKNCLEWFNOI4R844RI4R87465TREBFDDJ";
+    private long refreshExpirationTime = 2592000000L;
 
-    // Key expiration time (24 hours in milliseconds)
     private static final long JWT_EXPIRATION_TIME = 1000 * 60 * 60 * 24;
 
-    // Generate token with claims including shopId and shopCode
     public String generateToken(String username) {
         Map<String, Object> claims = new HashMap<>();
-
         return createToken(claims, username);
     }
 
-
-    public String generateSuperToken(String username) {
-        Map<String, Object> claims = new HashMap<>();
-
-        return createToken(claims, username);
-    }
-
-    // Create a JWT token
     private String createToken(Map<String, Object> claims, String username) {
         return Jwts.builder()
                 .setClaims(claims)
                 .setSubject(username)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + JWT_EXPIRATION_TIME)) // Expires in 24 hours
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_EXPIRATION_TIME))
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
-    // Get signing key
+    public String generateRefreshToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + refreshExpirationTime))
+                .signWith(getSigningKey(),SignatureAlgorithm.HS256)
+                .compact();
+    }
+
     private Key getSigningKey() {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
-    // Extract specific claims from the token
+//    private Key getSigningKey() {
+//        // Ensure the SECRET is at least 64 bytes long for HS512
+//        byte[] keyBytes = Decoders.BASE64.decode(SECRET); // Decode your base64 string into bytes
+//        if (keyBytes.length < 64) {
+//            throw new IllegalArgumentException("Key must be at least 64 bytes long for HS512.");
+//        }
+//        return Keys.hmacShaKeyFor(keyBytes);  // Use the key for HMAC-SHA512 signing
+//    }
+
     private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
 
-    // Extract all claims from the token
     private Claims extractAllClaims(String token) {
         return Jwts.parserBuilder()
                 .setSigningKey(getSigningKey())
@@ -69,25 +73,18 @@ public class JwtUtils {
                 .getBody();
     }
 
-    // Extract the username (subject) from the token
     public String extractUserName(String token) {
         return extractClaim(token, Claims::getSubject);
     }
 
-
-
-
-    // Extract the expiration date from the token
     public Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }
 
-    // Check if the token has expired
     private Boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
     }
 
-    // Validate if the token is valid (correct user and not expired)
     public Boolean isTokenValid(String token, UserDetails userDetails) {
         final String username = extractUserName(token);
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
